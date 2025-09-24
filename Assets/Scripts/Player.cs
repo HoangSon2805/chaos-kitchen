93e41e3ee82517ddaa72cf582de3eb4ff8776709
HoangSon2805 <234013544+HoangSon2805@users.noreply.github.com>
2025-09-24 07:41:37 +0700

    C# Event for Interact Action
    C# Event for Interact Action in Player.cs and GameInput.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,30 @@ public class Player : MonoBehaviour {
     private bool isWalking;
     private Vector3 lastInteractDir;
 
+    private void Start() {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        } 
+    }
+
     private void Update() {
         HandleMovement();
         HandleInteractions();
@@ -34,12 +58,9 @@ public class Player : MonoBehaviour {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 // Has ClearCounter
-                clearCounter.Interact();
+                
             }
-        } else
-        {
-            Debug.Log("-");
-        }
+        } 
 
     }
 
@@ -92,7 +113,7 @@ public class Player : MonoBehaviour {
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        Debug.Log(Time.deltaTime);
+        //Debug.Log(Time.deltaTime);
 
     }
 }
