c00f851cfc489ff5ef7dd5393cd5e5884947c51d
HoangSon2805 <234013544+HoangSon2805@users.noreply.github.com>
2025-09-24 10:22:27 +0700

    complete select object change visual

--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class SelectedCounterVisual : MonoBehaviour
 {
+    [SerializeField] private ClearCounter clearCounter;
+    [SerializeField] private GameObject visualGameObject;
     private void Start() {
         Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
     }
 
     private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e) {
-        throw new System.NotImplementedException();
+        if (e.selectedCounter == clearCounter)
+        {
+            Show();
+        } else { 
+            Hide();
+        }
+    }
+    private void Show() { 
+        visualGameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        visualGameObject.SetActive(false);
     }
 }
